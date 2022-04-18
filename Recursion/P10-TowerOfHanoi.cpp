2790e18788c2962f76b5a51713429a4bb6c081a2
Aayush <aayush5emails@gmail.com>
2022-04-18 19:34:12 +0530

    Tower Of Hanoi

---
+++ Recursion/P10-TowerOfHanoi.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+void TOH(int n, int a, int b, int c)
+{
+    if (n > 0)
+    {
+        TOH(n - 1, a, c, b);
+        cout << "(" << a << ',' << c << ")";
+        TOH(n - 1, b, a, c);
+    }
+}
+
+int main()
+{
+    TOH(4, 1, 2, 3);
+}
