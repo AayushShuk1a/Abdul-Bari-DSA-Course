5faa0e152d38a15de59d15224134a8675b223bc4
Aayush <aayush5emails@gmail.com>
2022-04-27 09:31:35 +0530

    differnece between two arrays

--- Array ADT/P11-SetOfOperations.cpp
+++ Array ADT/P11-SetOfOperations.cpp
@@ -31,7 +31,43 @@ public:
 
     void unions(int b[]);
     void intersection(int b[]);
+    void difference(int b[]);
 };
+
+void operations::difference(int b[])
+{
+    int i = 0, j = 0, k = 0;
+    int c[10];
+
+    while (i < 7 && j < 7)
+    {
+        if (A[i] < b[j])
+        {
+            c[k++] = A[i++];
+        }
+        else if (A[i] > b[j])
+        {
+            j++;
+        }
+        else
+        {
+            i++;
+            j++;
+        }
+    }
+
+    while (i < 7)
+    {
+        c[k++] = A[i++];
+    }
+
+    cout << endl
+         << "Array1-Array2 : ";
+    for (int i = 0; i < k; i++)
+    {
+        cout << c[i] << ",";
+    }
+}
 
 void operations::unions(int b[])
 {
@@ -125,4 +161,5 @@ int main()
 
     arr.intersection(c);
     arr.unions(c);
+    arr.difference(c);
 }
