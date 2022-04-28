b2ed1570e61daa19f998661f01905e0b8435622a
Aayush <aayush5emails@gmail.com>
2022-04-29 00:27:34 +0530

    Duplicates Unsorted array

--- Array ADT/P15-Duplicates.cpp
+++ Array ADT/P15-Duplicates.cpp
@@ -31,6 +31,7 @@ public:
     }
 
     void Duplicates();
+    void unsortedDuplicates();
 };
 
 void Array::Duplicates()
@@ -56,6 +57,31 @@ void Array::Duplicates()
     }
 }
 
+void Array::unsortedDuplicates()
+{
+    int c;
+    for (int i = 0; i < length; i++)
+    {
+        c = 1;
+        if (A[i] != -1)
+        {
+            for (int j = i + 1; j < length; j++)
+            {
+                if (A[i] == A[j])
+                {
+                    c++;
+                    A[j] = -1;
+                }
+            }
+            if (c > 1)
+            {
+                cout << endl
+                     << A[i] << " occurs " << c << " Times.";
+            }
+        }
+    }
+}
+
 int main()
 {
     Array arr(10);
@@ -68,5 +94,24 @@ int main()
 
     arr.display();
 
-    arr.Duplicates();
+    cout << endl
+         << "1.Sorted" << endl;
+    cout << "2.Unsorted" << endl;
+    int ch;
+    cin >> ch;
+
+    switch (ch)
+    {
+    case 1:
+        arr.Duplicates();
+        break;
+
+    case 2:
+        arr.unsortedDuplicates();
+        break;
+
+    default:
+        cout << "Enter Valid Choice";
+        break;
+    }
 }
