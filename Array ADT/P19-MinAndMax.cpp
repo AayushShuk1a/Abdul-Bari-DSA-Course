f57c5e10e5a1f1e1ef0bde503c024f40377f5e71
Aayush <aayush5emails@gmail.com>
2022-04-29 11:09:36 +0530

    Minimum and Maximum

---
+++ Array ADT/P19-MinAndMax.cpp
@@ -0,0 +1,72 @@
+#include <iostream>
+using namespace std;
+
+class Array
+{
+private:
+    int *A;
+    int size;
+    int length;
+
+public:
+    Array(int sz)
+    {
+        size = sz;
+        A = new int(size);
+        length = 0;
+    }
+
+    void fillArray(int value, int index)
+    {
+        A[index] = value;
+        length++;
+    }
+
+    void display()
+    {
+        for (int i = 0; i < length; i++)
+        {
+            cout << A[i] << ",";
+        }
+    }
+
+    void MinAndMax();
+};
+
+void Array::MinAndMax()
+{
+    int min = A[0];
+    int max = A[0];
+    for (int i = i; i < length; i++)
+    {
+        if (A[i] < min)
+        {
+            min = A[i];
+        }
+        else if (A[i] > max)
+        {
+            max = A[i];
+        }
+    }
+
+    cout << endl
+         << "Min is : " << min;
+    cout << endl
+         << "Max is : " << max;
+}
+
+int main()
+{
+
+    Array arr(10);
+    int n;
+    for (int i = 0; i < 10; i++)
+    {
+        cin >> n;
+        arr.fillArray(n, i);
+    }
+
+    arr.display();
+
+    arr.MinAndMax();
+}
