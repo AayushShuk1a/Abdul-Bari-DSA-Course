ffb159a27a7232c867a0f2b951602aa9568085f2
Aayush <aayush5emails@gmail.com>
2022-06-01 07:03:15 +0530

    Reverse Double Linked List

---
+++ Linked List/P17-ReverseDoubleLinkedList.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    Node *prev;
+    int data;
+    Node *next;
+};
+
+class ReverseDL
+{
+private:
+    Node *first;
+
+public:
+    ReverseDL(int A[], int n);
+    void Display();
+    void Reverse();
+};
+
+ReverseDL::ReverseDL(int A[], int n)
+{
+    Node *t, *last;
+    first = new Node;
+    first->data = A[0];
+    first->prev = NULL;
+    first->next = NULL;
+    last = first;
+    for (int i = 1; i < n; i++)
+    {
+        t = new Node;
+        t->data = A[i];
+        t->prev = last;
+        t->next = NULL;
+        last->next = t;
+        last = t;
+    }
+}
+
+void ReverseDL::Display()
+{
+    Node *p = first;
+    while (p != NULL)
+    {
+        cout << p->data << " ";
+        p = p->next;
+    }
+}
+
+void ReverseDL::Reverse()
+{
+    Node *p = first, *temp;
+    while (p != NULL)
+    {
+        temp = p->next;
+        p->next = p->prev;
+        p->prev = temp;
+        p = p->prev;
+        if (p && p->next == NULL)
+        {
+
+            first = p;
+        }
+    }
+}
+
+int main()
+{
+    int A[] = {1, 5, 7, 10, 15, 18, 21};
+
+    ReverseDL rev(A, sizeof(A) / sizeof(A[0]));
+
+    rev.Display();
+
+    rev.Reverse();
+    cout << endl
+         << "After Reverse :" << endl;
+
+    rev.Display();
+}
