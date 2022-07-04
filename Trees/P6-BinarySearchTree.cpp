0fc0fc3cdeef08d47430a607a1fbd0c37cdecb69
Aayush <aayush5emails@gmail.com>
2022-07-04 10:46:04 +0530

    Delete In BST

--- Trees/P6-BinarySearchTree.cpp
+++ Trees/P6-BinarySearchTree.cpp
@@ -24,7 +24,84 @@ public:
     void RInorder(Node *p);
     Node *GetRoot() { return root; }
     void SetRoot(Node *p) { root = p; }
+    Node *Delete(Node *p, int key);
+    int height(Node *p);
+    Node *InOrderPre(Node *p);
+    Node *InOrderSucc(Node *p);
 };
+
+Node *BST::InOrderPre(Node *p)
+{
+    while (p && p->Rchild != NULL)
+    {
+        p = p->Rchild;
+    }
+
+    return p;
+}
+
+Node *BST::InOrderSucc(Node *p)
+{
+    while (p && p->Lchild != NULL)
+    {
+
+        p = p->Lchild;
+    }
+
+    return p;
+}
+
+int BST::height(Node *p)
+{
+    int x, y;
+    if (p == NULL)
+        return 0;
+
+    x = height(p->Lchild);
+    y = height(p->Rchild);
+    return x > y ? x + 1 : y + 1;
+}
+
+Node *BST::Delete(Node *p, int key)
+{
+    Node *temp;
+    if (p == NULL)
+        return NULL;
+
+    if (p->Lchild == NULL && p->Rchild == NULL)
+    {
+        if (p == root)
+            root = NULL;
+
+        delete p;
+        return NULL;
+    }
+
+    if (key < p->data)
+    {
+        p->Lchild = Delete(p->Lchild, key);
+    }
+    else if (key > p->data)
+    {
+        p->Rchild = Delete(p->Rchild, key);
+    }
+    else
+    {
+        if (height(p->Lchild) > height(p->Rchild))
+        {
+            temp = InOrderPre(p->Lchild);
+            p->data = temp->data;
+            p->Lchild = Delete(p->Lchild, temp->data);
+        }
+        else
+        {
+            temp = InOrderSucc(p->Rchild);
+            p->data = temp->data;
+            p->Rchild = Delete(p->Rchild, temp->data);
+        }
+    }
+    return p;
+}
 
 void BST::RInorder(Node *p)
 {
@@ -148,14 +225,19 @@ void BST::Inorder()
 int main()
 {
     BST t;
-    Node *root = t.Rinsert(t.GetRoot(), 26);
+    Node *root = t.Rinsert(t.GetRoot(), 20);
     t.SetRoot(root);
-    t.Rinsert(t.GetRoot(), 21);
-    t.Rinsert(t.GetRoot(), 11);
-    t.Rinsert(t.GetRoot(), 19);
+    t.Rinsert(t.GetRoot(), 90);
     t.Rinsert(t.GetRoot(), 30);
-    t.Rinsert(t.GetRoot(), 15);
+    t.Rinsert(t.GetRoot(), 80);
+    t.Rinsert(t.GetRoot(), 40);
+    t.Rinsert(t.GetRoot(), 90);
 
+    t.RInorder(root);
+
+    t.Delete(t.GetRoot(), 30);
+    cout << endl
+         << "After Delete" << endl;
     t.RInorder(root);
 
     Node *search = t.search(12);
