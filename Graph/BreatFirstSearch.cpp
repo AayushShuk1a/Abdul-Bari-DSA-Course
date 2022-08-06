9a3aaabc094c9a3a6694c290cf388283f9325066
Aayush <aayush5emails@gmail.com>
2022-08-06 15:48:57 +0530

    Breadth First Search

--- Graph/BreatFirstSearch.cpp
+++ Graph/BreatFirstSearch.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 #include <queue>
 
-void BFS(int A[][6], int start, int n)
+void BFS(int A[][7], int start, int n)
 {
     int i = start;
     queue<int> q;
-    int visited[6] = {0};
+    int visited[7] = {0};
     cout << i << " ";
     visited[i] = 1;
     q.emplace(i);
@@ -16,11 +16,11 @@ void BFS(int A[][6], int start, int n)
         q.pop();
         for (int j = 1; j < n; j++)
         {
-            if (visited[j] == 0 && A[i][j] == 1)
+            if (A[i][j] == 1 && visited[j] == 0)
             {
 
                 cout << j << " ";
-                visited[i] = 1;
+                visited[j] = 1;
                 q.emplace(j);
             }
         }
@@ -29,7 +29,13 @@ void BFS(int A[][6], int start, int n)
 
 int main()
 {
-    int A[6][6] = {{0, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 1, 0}, {0, 0, 0, 1, 1, 1}, {0, 1, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}, {0, 0, 1, 0, 0, 0}};
+    int G[7][7] = {{0, 0, 0, 0, 0, 0, 0},
+                   {0, 0, 1, 1, 0, 0, 0},
+                   {0, 1, 0, 0, 1, 0, 0},
+                   {0, 1, 0, 0, 1, 0, 0},
+                   {0, 0, 1, 1, 0, 1, 1},
+                   {0, 0, 0, 0, 1, 0, 0},
+                   {0, 0, 0, 0, 1, 0, 0}};
 
-    BFS(A, 2, 6);
+    BFS(G, 1, 7);
 }
